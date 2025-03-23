c26d2ce8719fe6c5867f2c433442f866e77561bb
van-gomes <nninha.gomes@gmail.com>
2025-03-23 17:56:35 -0300

    Fix css trash button

--- js/constroiCard.js
+++ js/constroiCard.js
@@ -1,15 +1,17 @@
 export function constroiCard(id, name, description, price, image) {
-    const produto = document.createElement("li");
-    produto.className = "product-item";
-    produto.innerHTML = `
+  const produto = document.createElement("li");
+  produto.className = "product-item";
+  produto.innerHTML = `
     <img class="product_image" src="${image}" alt="${name}">
     <div class="product_info">
         <h3 class="product_name">${name}</h3>
         <p class="product_description">${description}</p>
-        <p class="product_price">R$ ${price}</p>
+        <div class="product_info_footer">
+          <p class="product_price">R$ ${price}</p>
         <button class="delete-button" data-id="${id}">
             <img src="./assets/excluir.png" class="trash-icon" alt="Um ícone de lixeira">
         </button>
+        </div>
     </div>`;
-    return produto;
+  return produto;
 }
